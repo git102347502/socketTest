dbdec4419286d9a53a1cff3eccc035f071364849
conch <102347501@qq.com>
2019-02-19 18:46:49 +0800

    default
    default

---
+++ SocketDemo/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace SocketDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxReceive = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxConncount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(7, 42);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(721, 404);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(7, 494);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(619, 73);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(632, 495);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxConncount);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Panel2.Controls.Add(this.textBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxReceive);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1116, 582);
+            this.splitContainer1.SplitterDistance = 372;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(8, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "接收消息区，当前接收缓冲区：";
+            // 
+            // textBoxReceive
+            // 
+            this.textBoxReceive.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxReceive.Location = new System.Drawing.Point(242, 8);
+            this.textBoxReceive.Name = "textBoxReceive";
+            this.textBoxReceive.Size = new System.Drawing.Size(86, 27);
+            this.textBoxReceive.TabIndex = 2;
+            this.textBoxReceive.Text = "1024";
+            this.textBoxReceive.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(343, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "字节";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(632, 534);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 32);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "清空";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(9, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(357, 524);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(12, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(249, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "当前连接客户端列表，最大连接数：";
+            // 
+            // textBoxConncount
+            // 
+            this.textBoxConncount.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxConncount.Location = new System.Drawing.Point(277, 7);
+            this.textBoxConncount.Name = "textBoxConncount";
+            this.textBoxConncount.Size = new System.Drawing.Size(78, 27);
+            this.textBoxConncount.TabIndex = 3;
+            this.textBoxConncount.Text = "200";
+            this.textBoxConncount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(8, 459);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "消息接收者：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "全部",
+            "客户端选中项"});
+            this.comboBox1.Location = new System.Drawing.Point(106, 455);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(157, 28);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1116, 582);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stocket通信";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxReceive;
+        private System.Windows.Forms.TextBox textBoxConncount;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SocketDemo/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections.Concurrent;
+using System.Net.Sockets;
+using System.Net;
+using System.Collections.Generic;
+
+namespace SocketDemo
+{
+    public partial class Form1 : Form
+    {
+        public Action<string> Msg;
+        public Action<string> MsgList;
+        public Action<string> DeteLists;
+        public SocketManager m_socket;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int receive;
+        private int conncount;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadConfig();
+            this.Msg = new Action<string>(ShowMsg);
+            this.MsgList = new Action<string>(ShowList);
+            this.DeteLists = new Action<string>(DeteList);
+
+            m_socket = new SocketManager(conncount, receive);
+            m_socket.Msg = this.Msg;
+            m_socket.MsgList = this.MsgList;
+            m_socket.DeteList = this.DeteLists;
+            m_socket.Init();
+            Msg.Invoke("启动Socket服务，端口号13909");
+            Msg.Invoke("当前最大连接数：200，缓冲区大小：1024字节" + "\r\n");
+            m_socket.Start(new IPEndPoint(IPAddress.Any, 13909));
+        }
+
+        private void ReadConfig()
+        {
+            conncount = int.Parse(textBoxConncount.Text);
+            receive = int.Parse(textBoxReceive.Text);
+        }
+
+        public void ShowMsg(string value)
+        {
+            this.Invoke(new Action(() =>
+            {
+                textBox1.AppendText(value+ "\r\n");
+            }));
+        }
+        public void ShowList(string value)
+        {
+            this.Invoke(new Action(() =>
+            {
+                listBox1.Items.Add(value);
+            }));
+        }
+        public void DeteList(string value)
+        {
+            this.Invoke(new Action(() =>
+            {
+                listBox1.Items.Remove(value);
+            }));
+        }
+
+
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //发送给全部
+            if (comboBox1.SelectedIndex==0)
+            {
+                Msg.Invoke(DateTime.Now + "：主机发送给全部客户端消息\r\n" + textBox2.Text+ "\r\n");
+                for (int i = 0; i < m_socket.m_clients.Count; i++)
+                {
+                    m_socket.SendMessage(m_socket.m_clients[i], System.Text.Encoding.Default.GetBytes(textBox2.Text));
+                }
+            }
+            else
+            {
+                if (listBox1.SelectedItem!=null)
+                {
+                    var result = m_socket.m_clients.Find(c => c.Remote.ToString() == listBox1.SelectedItem.ToString());
+                    if (result != null)
+                    {
+                        m_socket.SendMessage(result, System.Text.Encoding.Default.GetBytes(textBox2.Text));
+                        Msg.Invoke(DateTime.Now + "：主机发送给客户端："+result.Remote+"消息\r\n"+textBox2.Text+ "\r\n");
+                        return;
+                    }
+                    MessageBox.Show("未搜索到选择客户端信息");
+                }
+                else
+                {
+                    MessageBox.Show("请在左侧选择需要发送的客户端");
+                }
+            }
+        }
+    }
+}
